6ba0ebe7250715636d99e713992e6b7ffbbdaec1
saulo <saulocn@gmail.com>
2016-08-12 08:33:03 -0300

    - Primeira requisição GET em '/v1/fotos'; - Retornando um JSON com a lista de fotos;

--- alurapic/config/express.js
+++ alurapic/config/express.js
@@ -5,5 +5,17 @@ var app = express();
 //Configurações do express
 app.use(express.static('./public'));//a partir da raíz do projeto
 
+app.get('/v1/fotos', function(req, res){
+	console.log("Tentando obter as fotos?");
+
+
+    var fotos = [
+        {_id: 1, titulo: 'Leão', url:'http://www.fundosanimais.com/Minis/leoes.jpg' },
+        {_id: 2, titulo: 'Leão 2', url:'http://www.fundosanimais.com/Minis/leoes.jpg' }
+    ];
+    res.json(fotos);
+	//res.end("Fotos!");
+});
+
 //exportando, tornando público minha variável app (instância do express)
 module.exports = app;
