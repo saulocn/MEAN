7320932584404cadcba615bcbef5ad7917e282a3
saulo <saulocn@gmail.com>
2016-08-12 11:31:57 -0300

    - Agrupando rotas semelhantes; - Utilzação do verbo delete; - Retorno de Status;

--- alurapic/app/api/foto.js
+++ alurapic/app/api/foto.js
@@ -1,14 +1,45 @@
 
 var api = {};
 
-
-api.lista = function(req, res){
-	console.log("Tentando obter as fotos?");
-	var fotos = [
+var fotos = [
 	{_id: 1, titulo: 'Leão', url:'http://www.fundosanimais.com/Minis/leoes.jpg' },
 	{_id: 2, titulo: 'Leão 2', url:'http://www.fundosanimais.com/Minis/leoes.jpg' }
 	];
+
+api.lista = function(req, res){
+	console.log("Tentando obter as fotos?");
+	
 	res.json(fotos);
 }
+
+
+api.buscaPorId = function(req, res){
+
+	// express mapeia o parâmetro, obtendo com o mesmo nome do parâmetro da url
+	var id = req.params.idFoto;
+
+	var foto = fotos.find(function(foto){
+		if(foto._id == id){
+			return true;
+		}
+	});
+	
+	res.json(foto);
+}
+
+
+api.removePorId = function(req, res){
+
+	// express mapeia o parâmetro, obtendo com o mesmo nome do parâmetro da url
+	var id = req.params.idFoto;
+
+	fotos = fotos.filter(function(foto){
+		return foto._id != id; 
+	});
+
+	res.sendStatus(204);
+	//mesma coisa que res.status(204).end();
+}
+
 
 module.exports = api;
--- alurapic/app/routes/foto.js
+++ alurapic/app/routes/foto.js
@@ -4,4 +4,20 @@ module.exports = function(app){
 	//app.get('/v1/fotos', api.lista);
 	var fotoApi = app.api.foto;
 	app.get('/v1/fotos', fotoApi.lista);
+
+
+	app.route('/v1/fotos/:idFoto')
+		.get(fotoApi.buscaPorId)
+		.delete(fotoApi.removePorId);
+	//app.get('/v1/fotos/:idFoto', fotoApi.buscaPorId);
+	//app.delete('/v1/fotos/:idFoto', fotoApi.removePorId);
+
+
+
+
+	/*app.get('/v1/fotos/:id/tipo/:idTipo', function(req, res) {
+		console.log(req.params.id+" - "+req.params.idTipo);
+		res.end(req.params.id+" - "+req.params.idTipo);
+   		// será que funciona?
+	});*/
 };
